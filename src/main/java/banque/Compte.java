209243942643afee248c28a608c5cc39fa62c6dc
dec <decleggal@gmail.com>
2025-01-28 16:10:34 +0100

    updated javadoc

--- src/main/java/banque/Compte.java
+++ src/main/java/banque/Compte.java
@@ -5,17 +5,21 @@ import banque.exceptions.SoldeInsuffisantException;
 public class Compte {
 
     private final double numero;
-
     private final String nomTitulaire;
-
     private final String prenomTitulaire;
-
     private final String addrTitulaire;
-
     private double solde;
-
     private final double decouvertMax;
 
+    /**
+     * Constructeur d'un compte
+     * @param numero Le numéro du compte
+     * @param nomTitulaire Le nom du titulaire
+     * @param prenomTitulaire Le prénom du titulaire
+     * @param addrTitulaire L'adresse du titulaire
+     * @param decouvertMax Le découvert maximal autorisé
+     * @param solde Le solde du compte
+     */
     public Compte(double numero, String nomTitulaire, String prenomTitulaire, String addrTitulaire, double decouvertMax, double solde) {
         this.numero = numero;
         this.nomTitulaire = nomTitulaire;
@@ -25,6 +29,14 @@ public class Compte {
         this.solde = solde;
     }
 
+    /**
+     * Constructeur d'un compte avec un solde initial de 0
+     * @param numero Le numéro du compte
+     * @param nomTitulaire Le nom du titulaire
+     * @param prenomTitulaire Le prénom du titulaire
+     * @param addrTitulaire L'adresse du titulaire
+     * @param decouvertMax Le découvert maximal autorisé
+     */
     public Compte(double numero, String nomTitulaire, String prenomTitulaire, String addrTitulaire, double decouvertMax) {
         this(numero, nomTitulaire, prenomTitulaire, addrTitulaire, decouvertMax, 0);
     }
@@ -33,7 +45,8 @@ public class Compte {
 
     /**
      * Credite le compte du montant passé en paramètre
-     * @param montant : La somme à créditer
+     * @param montant La somme à créditer
+     * @throws IllegalArgumentException Si le montant à créditer est négatif
      */
     public void crediter(double montant) {
         if(montant < 0) {
@@ -45,7 +58,9 @@ public class Compte {
 
     /**
      * Débite le compte du montant passé en paramètre
-     * @param montant : La somme à débiter
+     * @param montant La somme à débiter
+     * @throws SoldeInsuffisantException Si le solde est insuffisant
+     * @throws IllegalArgumentException Si le montant à débiter est négatif
      */
     public void debiter(double montant) throws SoldeInsuffisantException{
         if(montant < 0) {
@@ -61,8 +76,8 @@ public class Compte {
 
     /**
      * Vire le montant passé en paramètre du compte courant vers le compte destination
-     * @param montant : La somme à virer
-     * @param destination : Le compte de destination
+     * @param montant La somme à virer
+     * @param destination Le compte de destination
      */
     public void virerVers(double montant, Compte destination) {
         this.debiter(montant);
@@ -70,8 +85,8 @@ public class Compte {
     }
 
     /**
-     * Retourne vrai si le compte est à découvert
-     * @return boolean
+     * Détermine si le compte est à découvert
+     * @return Vrai si le compte est à découvert
      */
     public boolean estDecouvert() {
         return this.solde < 0;
